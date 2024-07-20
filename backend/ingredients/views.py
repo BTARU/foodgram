798984692e69c076b952b0608addfd97601a6712
Bulat Ayupov <sulamar66@gmail.com>
2024-07-20 19:44:38 +0500

    Remove ingredient filter

--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -1,20 +1,8 @@
-from django_filters.rest_framework import (
-    DjangoFilterBackend, FilterSet, CharFilter
-)
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-
 from .models import Ingredient
 from .serializers import IngredientSerializer
-
-
-class IngredientFilter(FilterSet):
-    name = CharFilter(max_length=128)
-
-    class Meta:
-        model = Ingredient
-        fields = ['name']
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
@@ -22,6 +10,3 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = IngredientSerializer
     permission_classes = (AllowAny,)
     pagination_class = None
-    filter_backends = (DjangoFilterBackend,)
-    # filterset_fields = ('name',)
-    filterset_class = IngredientFilter
