ca9fc87dabde9af0b3b4bf40c9a80b9ec36d9f20
Bulat Ayupov <sulamar66@gmail.com>
2024-07-26 17:01:46 +0500

    Review changes 6

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth import get_user_model, update_session_auth_hash
-from djoser import utils
-from djoser.compat import get_user_email
-from djoser.conf import settings
+from django.contrib.auth import get_user_model
 from djoser.serializers import SetPasswordSerializer, UserCreateSerializer
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
@@ -49,24 +46,15 @@ class UserViewSet(
         detail=False,
         permission_classes=[IsAuthenticated]
     )
-    def set_password(self, request, *args, **kwargs):
+    def set_password(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        self.request.user.set_password(serializer.data['new_password'])
-        self.request.user.save()
-
-        if settings.PASSWORD_CHANGED_EMAIL_CONFIRMATION:
-            context = {'user': self.request.user}
-            to = [get_user_email(self.request.user)]
-            settings.EMAIL.password_changed_confirmation(
-                self.request, context).send(to)
-
-        if settings.LOGOUT_ON_PASSWORD_CHANGE:
-            utils.logout_user(self.request)
-        elif settings.CREATE_SESSION_ON_LOGIN:
-            update_session_auth_hash(self.request, self.request.user)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        request.user.set_password(serializer.data['new_password'])
+        request.user.save()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
 
     @action(
         ['put'],
