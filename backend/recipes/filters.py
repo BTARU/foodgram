152870f7a66e181b02525b00e5c8767a1ba6dc43
Bulat Ayupov <sulamar66@gmail.com>
2024-07-30 02:38:23 +0500

    Filters fix 2

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -18,24 +18,19 @@ class RecipeFilter(FilterSet):
     def filter_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if user.is_authenticated and value == '1':
-            recipes_in_shop_cart = user.user_shopping_cart_recipes.all()
-            if not recipes_in_shop_cart:
-                return recipes_in_shop_cart
-            for user_recipe_in_shop_cart in recipes_in_shop_cart:
-                queryset = queryset.filter(
-                    id=user_recipe_in_shop_cart.recipe.id
-                )
-            return queryset
+            recipes_in_shop_cart = user.user_shopping_cart_recipes.values_list(
+                'recipe__id',
+                flat=True
+            )
+            return queryset.filter(id__in=recipes_in_shop_cart)
         return queryset
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if user.is_authenticated and value == '1':
-            favorite_recipes = user.user_favorite_recipes.all()
-            if not favorite_recipes:
-                return favorite_recipes
-            for user_favorite_recipe in favorite_recipes:
-                queryset = queryset.filter(
-                    id=user_favorite_recipe.recipe.id
-                )
+            favorite_recipes = user.user_favorite_recipes.values_list(
+                'recipe__id',
+                flat=True
+            )
+            return queryset.filter(id__in=favorite_recipes)
         return queryset
